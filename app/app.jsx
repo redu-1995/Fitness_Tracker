cd9b82400423df2af5b41425cdd28b5f62ecc25c
redu-1995 <nara1415karol@gmail.com>
2026-05-23 10:34:21 +0300

    creating a header in homescreen

---
+++ app/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import HomeScreen from '../src/screens/HomeScreen'
+
+export default function App(){
+
+    return <HomeScreen />
+        
+}
+
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, Text, TextInput, Button, ScrollView, StyleSheet, 
+    SafeAreaView
+ } from 'react-native'
+
+export default function HomeScreen(){
+
+    return (
+     <SafeAreaView
+ style = {styles.safeArea}> 
+        <ScrollView contentContainerStyle = {styles.scrollContainer}>
+            <View style={styles.headerContainer}>
+                 <Text style = {styles.title}>Good Morning</Text>
+                 <Text style = {styles.subtitle}>Ready for today's workout?</Text>
+            </View>
+            
+        </ScrollView>     
+     </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+  safeArea :{
+    flex: 1,
+    backgroundColor: '#fff'
+    
+  },
+  scrollContainer:{
+    paddingHorizontal: 20,
+    paddingTop: 20
+  },
+  headerContainer:{
+    marginBottom: 24,
+    
+  },
+  title: {
+    fontSize: 28,
+    fontWeight:'bold',
+    color:'#1a1a1a'
+  },
+  subtitle:{
+    fontSize:18,
+    marginTop: 10,
+    color: '#666'
+  }
+})
